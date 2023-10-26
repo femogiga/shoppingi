df0d922de61be9a9ac7774ad009dacf6c64090f2
femogiga <femogiga@yahoo.com>
2023-10-26 14:43:45 +0100

    alpha 1.49

--- server/index.js
+++ server/index.js
@@ -6,14 +6,14 @@ const productRoute = require('./routes/productRoute');
 const categoryRoute = require('./routes/categoryRoute');
 const shoppingListRoute = require('./routes/shoppingListRoute');
 //const { shoppingList } = require('./controllers/prismaClient');
-
+//
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
 app.use('/products', productRoute);
 app.use('/category', categoryRoute);
-app.use('/shoppingList',shoppingListRoute);
+app.use('/shoppingList', shoppingListRoute);
 const port = process.env.PORT || 8000;
 
 app.use('/', (req, res) => {
